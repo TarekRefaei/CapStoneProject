5de3d3a39a066425ddad34ef025679a36ac80925
Tarek Refaei <tootaa1990@yahoo.com>
2023-07-31 19:41:21 -0400

    Part1

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,15 +16,6 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
-
-# Create an `about` view to render a static about page
-# def about(request):
-# ...
-
-
-# Create a `contact` view to return a static contact page
-#def contact(request):
-
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
@@ -37,12 +28,20 @@ logger = logging.getLogger(__name__)
 # def registration_request(request):
 # ...
 
-# Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
 
+def aboutUs(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about.html', context)
+
+def contactUs(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
